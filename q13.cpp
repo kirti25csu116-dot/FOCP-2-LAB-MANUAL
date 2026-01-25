aa8214e7492689ea94fb1717a615eccdb07ca9bd
kirti25csu116-dot <kirti25csu116@ncuindia.edu>
2026-01-25 16:27:21 +0530

    Add files via upload

---
+++ q13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int d;
+    int b,a,c;
+    cout<<"enter b";
+    cin>>b;
+    cout<<"enter a";
+    cin>>a;
+    cout<<"enter c";
+    cin>>c;
+    d = b*b-4*a*c;
+    cout<<"the value of discriminant is"<<d;
+    return 0;
+
+}
